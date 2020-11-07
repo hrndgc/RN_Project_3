811f6ee320a29b4c6f7542327d2937c73c46ba20
Harun <67702854+hrndgc@users.noreply.github.com>
2020-11-07 19:13:20 +0300

    Updated some pages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import Router from './src/Router';
 import {name as appName} from './app.json';
--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {SafeAreaView, View, Text, FlatList} from 'react-native';
 
+import auth from '@react-native-firebase/auth';
 import {timelinePage} from './styles';
 import {PostItem, PostInput, Header, TopicSelectModal} from '../components';
 
 const Timeline = () => {
+  const user = auth.currentUser;
+  
   return (
     <SafeAreaView style={timelinePage.container}>
       <View style={timelinePage.container}>
